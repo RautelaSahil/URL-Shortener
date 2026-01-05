c745c96b6cd48c97cc36638a5b912b7ec7588fe5
Negi-Apoorv <negiapoorv41828@gmail.com>
2026-01-05 20:13:51 +0530

    Update db.py

--- backend/db.py
+++ backend/db.py
@@ -9,7 +9,34 @@ def get_connection():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME"),
-        port=3306,
-        auth_plugin="caching_sha2_password"
+        database=os.getenv("DB_NAME")
     )
+
+def insert_url(original, pipiurl):
+    conn= get_connection()
+    cursor= conn.cursor()
+    
+    query= "insert into dmforlink (original, pipiurl) values (%s, %s)"
+        
+    cursor.execute(query,(original, pipiurl))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def get_original(pipiurl):
+    conn= get_connection()
+    cursor= conn.cursor()
+    
+    query= "select original from dmforlink where pipiurl= %s"
+        
+    cursor.execute(query,( pipiurl,))
+    
+    result= cursor.fetchone()
+    
+    cursor.close()
+    conn.close()
+    
+    if result:
+        return result[0]
+    return None
+
